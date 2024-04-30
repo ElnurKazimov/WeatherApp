49a17e0ac1789133a1e0e71b34b820a9ad8a192d
ElnurKazimov <ekazimov009@gmail.com>
2024-04-30 21:34:32 +0400

    someUpdate

--- script.js
+++ script.js
@@ -7,43 +7,58 @@ const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 const emojiGif = document.querySelector(".emoji");
+const card = document.querySelector(".card")
 
 async function checkWeather(city){
    const response = await fetch(apiUrl + city + `&appid=${apiKey}`); 
    var data = await response.json();
+
+   console.log(data)
      
    document.querySelector(".city").innerHTML = data.name;
    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)  + "°C";
-   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-   document.querySelector(".wind").innerHTML = data.wind.speed + " km/h"  ;
-   
-   console.log(data)
+   document.querySelector(".humidity").innerHTML = "<p>Rütubət:</p>"  + data.main.humidity + "%";
+   document.querySelector(".wind").innerHTML = "<p>Külək sürəti:</p>" + data.wind.speed + " km/h"  ;
+   document.querySelector(".max").innerHTML = "<p>Maximum: </p>" + Math.round(data.main.temp_max) + "°C"
+   document.querySelector(".min").innerHTML = "<p>Minimum: </p>" + Math.round(data.main.temp_min) + "°C"
 
+  if(data.dt > data.sys.sunset){
+    card.style.background = "url(image/night2.gif)"
+   
+  }
+  else{
+    card.style.background = "linear-gradient(135deg, #00feba, #5b548a)";
+  }
 
    if(data.weather[0].main == "Clouds"){
-       weatherIcon.src = "image/cloudy4.png";
-       emojiGif.src = "image/512.gif"
+    document.querySelector(".sun").src = "image/cloud.png"
    }
 
-  else if(data.weather[0].main == "Clear"){
-       weatherIcon.src = "image/clear.jpg";
-       emojiGif.src = "image/happy.gif";
+   
+
+  else if(data.weather[0].main == "Clear" && data.dt > data.sys.sunset ){
+       document.querySelector(".sun").src = "image/moonNight.webp"
+   }
+
+   else if(data.weather[0].main == "Clear"){
+    document.querySelector(".sun").src ="image/sun.png"
    }
 
  else if(data.weather[0].main == "Rain"){
-       weatherIcon.src = "image/rain.gif";
-       emojiGif.src = "image/worried.gif";
+    document.querySelector(".sun").src ="image/rain.gif"
+     
    }
 
+ 
+
 else if(data.weather[0].main == "Drizzle"){
-       weatherIcon.src = "image/drizzle.png";
-       emojiGif.src = "image/worried.gif";
+       
    }
 
    else if(data.weather[0].main == "Snow"){
-       weatherIcon.src = "image/snowflake2.gif " ;
-       emojiGif.src = "image/cold2.gif";
+       
    }
+
 
    document.querySelector(".weather").style.display = "block";
 }  
@@ -51,3 +66,6 @@ else if(data.weather[0].main == "Drizzle"){
 searchBtn.addEventListener("click", ()=>{
     checkWeather(searchBox.value);
 });
+
+
+
